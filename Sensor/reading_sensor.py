df3575e8f6278eabfcf48993aa53e1cdec3a1875
Spruudis <r.spuris@gmail.com>
2020-02-04 21:45:12 +0000

    whoop 2

--- Sensor/reading_sensor.py
+++ Sensor/reading_sensor.py
@@ -93,7 +93,7 @@ def main():
         if status & 1:
             error = bus.read_byte_data(CSS811_DEVICE_ADDRESS, CSS811_ERROR_ID)
             print("    There is an error on the I²C or sensor after changing to application mode:", error)
-            print(interpret_error(error)))
+            print(interpret_error(error))
 
 
 
