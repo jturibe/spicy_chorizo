01b02808f3328a39baf40ef7448328566010b792
Neil Richardson <nrich98@googlemail.com>
2020-02-05 16:47:39 +0000

    Update Temp_sensor.py

--- Sensor/Temp_sensor.py
+++ Sensor/Temp_sensor.py
@@ -3,9 +3,12 @@ import time
 
 bus = smbus.SMBus(1)
 while True:
-    # bus.write_byte(0x40, 0xF5)
-    # time.sleep(2)
-    # print("Humidity: ",  bus.read_i2c_block_data(0x40, 0xE7,2))
+    bus.write_byte(0x40, 0xE5)
+    time.sleep(2)
+    hmb, hlb = bus.read_i2c_block_data(0x40, 0xE5,2)
+    hmb = hmb << 8
+    hum = 125*(hmb + hlb) / 65536 - 6
+    print("RH% = ", hum)
     bus.write_byte(0x40, 0xE3)
     time.sleep(2)
     mb, lb, cm = bus.read_i2c_block_data(0x40, 0xE3, 3)
