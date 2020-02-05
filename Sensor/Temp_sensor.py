15db61b2ee4af7f9196d8afaa80e20bcf95e963d
Neil Richardson <nrich98@googlemail.com>
2020-02-05 16:40:46 +0000

    Update Temp_sensor.py

--- Sensor/Temp_sensor.py
+++ Sensor/Temp_sensor.py
@@ -8,7 +8,7 @@ while True:
     # print("Humidity: ",  bus.read_i2c_block_data(0x40, 0xE7,2))
     bus.write_byte(0x40, 0xE3)
     time.sleep(2)
-    mb, lb, cm = bus.read_i2c_block_data(0x40, 0xE3, 2)
+    mb, lb, cm = bus.read_i2c_block_data(0x40, 0xE3, 3)
     mb = mb << 8
     temp = (mb + lb)*175.72 / 65536 - 46.85
     print("Temperature(Celsius): ", temp)
