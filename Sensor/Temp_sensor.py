d7114db0002eef6904a89bfb72ee0fca760fdaa3
Neil Richardson <nrich98@googlemail.com>
2020-02-05 17:43:18 +0000

    Update Temp_sensor.py

--- Sensor/Temp_sensor.py
+++ Sensor/Temp_sensor.py
@@ -1,10 +1,21 @@
 import smbus
 import time
+import json
+import paho.mqtt.client as mqtt
+
 
 bus = smbus.SMBus(1)
+
+client = mqtt.Client()
+con_code = client.connect("test.mosquitto.org",port=1883)
+
+if not con_code:
+    client.publish("IC.embeddedspicy_chorizo/test","hello")
+else print(mqtt.error_string(con_code))
+
 while True:
     bus.write_byte(0x40, 0xE5)
-    time.sleep(2)
+    time.sleep(5)
     hmb, hlb = bus.read_i2c_block_data(0x40, 0xE5,2)
     hmb = hmb << 8
     hum = 125*(hmb + hlb) / 65536 - 6
@@ -15,3 +26,14 @@ while True:
     mb = mb << 8
     temp = (mb + lb)*175.72 / 65536 - 46.85
     print("Temperature(Celsius): ", temp)
+
+
+    hum_out = float(hmb)
+    temp_out = float(temp)
+
+    thisdict = {
+      "temperature": temp_out,
+      "humidity": hum_out
+    }
+
+    message = json.dump(thisdict)
