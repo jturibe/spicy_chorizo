bf1ed5f03b203dc2f92707583ea6ae1244849c4c
Neil Richardson <nrich98@googlemail.com>
2020-02-05 15:34:57 +0000

    Update Temp_sensor.py

--- Sensor/Temp_sensor.py
+++ Sensor/Temp_sensor.py
@@ -2,6 +2,10 @@ import smbus
 import time
 
 bus = smbus.SMBus(1)
-bus.write_byte(0x40, 0xF5)
-time.sleep(2)
-print(bus.read_byte(0x40))
+while True:
+    bus.write_byte(0x40, 0xF5)
+    time.sleep(2)
+    print("Humidity: ", bus.read_byte(0x40))
+    bus.write_byte(0x40, 0xE0)
+    time.sleep(2)
+    print("Temperature: ", bus.read_byte(0x40))
