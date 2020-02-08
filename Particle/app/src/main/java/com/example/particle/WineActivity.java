7d375ae3a26cd279c75e3c0ece17197856e76b1d
Javier Torres <jt3917@ic.ac.uk>
2020-02-08 19:11:15 +0000

    Added app connections

---
+++ Particle/app/src/main/java/com/example/particle/WineActivity.java
@@ -0,0 +1,14 @@
+package com.example.particle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wine);
+    }
+}
