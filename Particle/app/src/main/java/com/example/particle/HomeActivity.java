4900f5d2ffa30ab7c889a18b87a088d70f38bdb1
Neil Richardson <nrich98@googlemail.com>
2020-02-08 22:50:39 +0000

    adding push notifications from server

--- Particle/app/src/main/java/com/example/particle/HomeActivity.java
+++ Particle/app/src/main/java/com/example/particle/HomeActivity.java
@@ -7,12 +7,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import static android.content.ContentValues.TAG;
 
@@ -23,7 +29,8 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        basicReadWrite();
+        FirebaseMessaging.getInstance().subscribeToTopic("emergency_updates");
+        updateStats();
         settingsButton = findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,14 +41,11 @@ public class HomeActivity extends Activity {
         });
     }
 
-    public void basicReadWrite() {
-        // [START write_message]
-        // Write a message to the database
+    public void updateStats() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference tempRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/current_measurement/temperature");
         DatabaseReference humidRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/current_measurement/humidity");
         DatabaseReference lightRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/current_measurement/light");
-        // [START read_message]
         // Read from the database
         tempRef.addValueEventListener(new ValueEventListener() {
             @Override
--- Server/server.py
+++ Server/server.py
@@ -8,6 +8,7 @@ import numpy as np
 import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials
+from firebase_admin import messaging
 
 
 # Connect to Firebase
