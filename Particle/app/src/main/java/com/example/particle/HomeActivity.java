6ab0c4093b29d57cb00949dcc4c1fa0b7f337881
Neil Richardson <nrich98@googlemail.com>
2020-02-11 15:16:32 +0000

    We can pull graphs from firebase

--- Particle/app/src/main/java/com/example/particle/HomeActivity.java
+++ Particle/app/src/main/java/com/example/particle/HomeActivity.java
@@ -2,9 +2,13 @@ package com.example.particle;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -25,12 +29,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.content.ContentValues.TAG;
 
 public class HomeActivity extends Activity {
 
     ImageButton settingsButton;
+    LinearLayout temperaturePanel;
+    LinearLayout humidityPanel;
     ViewPager viewPager;
+    boolean temp_graphs;
+    boolean hum_graphs;
+    ViewPagerAdapter viewPagerAdapter;
     LinearLayout sliderDotspanel;
     private int dotscount;
     private ImageView[] dots;
@@ -38,6 +51,8 @@ public class HomeActivity extends Activity {
     int value = 0;
 
     public void onCreate(Bundle savedInstanceState){
+        temp_graphs = true;
+        hum_graphs = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         FirebaseMessaging.getInstance().subscribeToTopic("emergency_updates");
@@ -55,63 +70,176 @@ public class HomeActivity extends Activity {
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
 
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+//        initialise_Graphs();
 
-        viewPager.setAdapter(viewPagerAdapter);
+//        dotscount = viewPagerAdapter.getCount();
+//        dots = new ImageView[dotscount];
+//
+//        for (int i = 0; i < dotscount; i++) {
+//
+//            dots[i] = new ImageView(this);
+//            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+//                    R.drawable.nonactive_dot));
+//
+//            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+//                    (LinearLayout.LayoutParams.WRAP_CONTENT,
+//                            LinearLayout.LayoutParams.WRAP_CONTENT);
+//
+//            params.setMargins(8,0,8,0);
+//
+//            sliderDotspanel.addView(dots[i], params);
+//        }
+//
+//        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+//                R.drawable.active_dot));
+//
+//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//
+//                for (int i = 0; i < dotscount; i++) {
+//                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+//                            R.drawable.nonactive_dot));
+//                }
+//
+//                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+//                        R.drawable.active_dot));
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
 
-        dotscount = viewPagerAdapter.getCount();
-        dots = new ImageView[dotscount];
-
-        for (int i = 0; i < dotscount; i++) {
-
-            dots[i] = new ImageView(this);
-            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
-                    R.drawable.nonactive_dot));
-
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
-                    (LinearLayout.LayoutParams.WRAP_CONTENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT);
-
-            params.setMargins(8,0,8,0);
-
-            sliderDotspanel.addView(dots[i], params);
-        }
-
-        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
-                R.drawable.active_dot));
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        temperaturePanel = findViewById(R.id.temp_panel);
+        temperaturePanel.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            public void onClick(View v) {
+                temp_graphs = true;
+                hum_graphs = false;
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference tempGraphsRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/graphs_temp");
+                tempGraphsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        ArrayList<Bitmap> image_bitmaps = new ArrayList<>();
+                        for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                            String image_string = postSnapshot.getValue(String.class);
 
+                            byte[] decodedString = Base64.decode(image_string.getBytes(), Base64.DEFAULT);
+                            Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                            image_bitmaps.add(image);
+                        }
+
+                        viewPagerAdapter = new ViewPagerAdapter(image_bitmaps, HomeActivity.this);
+                        viewPager.setAdapter(viewPagerAdapter);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // ...
+                    }
+                });
             }
+        });
 
+        humidityPanel = findViewById(R.id.hum_panel);
+        humidityPanel.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageSelected(int position) {
+            public void onClick(View v) {
+                temp_graphs = false;
+                hum_graphs = true;
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference humGraphsRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/graphs_hum");
+                humGraphsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        ArrayList<Bitmap> image_bitmaps = new ArrayList<>();
+                        for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                            String image_string = postSnapshot.getValue(String.class);
 
-                for (int i = 0; i < dotscount; i++) {
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
-                            R.drawable.nonactive_dot));
-                }
+                            byte[] decodedString = Base64.decode(image_string.getBytes(), Base64.DEFAULT);
+                            Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                            image_bitmaps.add(image);
+                        }
 
-                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
-                        R.drawable.active_dot));
+                        viewPagerAdapter = new ViewPagerAdapter(image_bitmaps, HomeActivity.this);
+                        viewPager.setAdapter(viewPagerAdapter);
+                    }
 
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // ...
+                    }
+                });
 
             }
         });
 
     }
 
+//    private void initialise_Graphs() {
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference tempGraphsRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/graphs_temp");
+//        tempGraphsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                ArrayList<Bitmap> image_bitmaps = new ArrayList<>();
+//                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+//                    String image_string = postSnapshot.getValue(String.class);
+//
+//                    byte[] decodedString = Base64.decode(image_string.getBytes(), Base64.DEFAULT);
+//                    Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+//                    image_bitmaps.add(image);
+//                }
+//
+//                viewPagerAdapter = new ViewPagerAdapter(image_bitmaps, HomeActivity.this);
+//                viewPager.setAdapter(viewPagerAdapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                // ...
+//            }
+//        });
+//
+//        dotscount = viewPagerAdapter.getCount();
+//        dots = new ImageView[dotscount];
+//
+//        for (int i = 0; i < dotscount; i++) {
+//
+//            dots[i] = new ImageView(this);
+//            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+//                    R.drawable.nonactive_dot));
+//
+//            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+//                    (LinearLayout.LayoutParams.WRAP_CONTENT,
+//                            LinearLayout.LayoutParams.WRAP_CONTENT);
+//
+//            params.setMargins(8,0,8,0);
+//
+//            sliderDotspanel.addView(dots[i], params);
+//        }
+//
+//        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),
+//                R.drawable.active_dot));
+//
+//    }
+
     public void updateStats() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference tempRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/current_measurement/temperature");
         DatabaseReference humidRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/current_measurement/humidity");
         DatabaseReference lightRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/current_measurement/light");
+        DatabaseReference tempGraphsRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/graphs_temp");
+        DatabaseReference humGraphsRef = database.getReferenceFromUrl("https://spicychorizo-794f1.firebaseio.com/graphs_hum");
         // Read from the database
         tempRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -157,6 +285,64 @@ public class HomeActivity extends Activity {
                 TextView lighting= findViewById(R.id.current_light);
                 String light_text = value;
                 lighting.setText(light_text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        tempGraphsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(temp_graphs){
+                    ArrayList<Bitmap> image_bitmaps = new ArrayList<>();
+                    for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                        String image_string = postSnapshot.getValue(String.class);
+
+                        byte[] decodedString = Base64.decode(image_string.getBytes(), Base64.DEFAULT);
+                        Log.w(TAG, image_string);
+                        Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                        if (image == null){
+                            Log.w(TAG, "Urine trouble");
+                        }
+                        Log.w(TAG, "after bitmap factory");
+                        image_bitmaps.add(image);
+                    }
+
+                    viewPagerAdapter = new ViewPagerAdapter(image_bitmaps, HomeActivity.this);
+                    viewPager.setAdapter(viewPagerAdapter);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        humGraphsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(hum_graphs){
+                    ArrayList<Bitmap> image_bitmaps = new ArrayList<>();
+                    for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                        String image_string = postSnapshot.getValue(String.class);
+
+                        byte[] decodedString = Base64.decode(image_string, Base64.DEFAULT);
+                        Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                        image_bitmaps.add(image);
+                    }
+
+                    viewPagerAdapter = new ViewPagerAdapter(image_bitmaps, HomeActivity.this);
+                    viewPager.setAdapter(viewPagerAdapter);
+                }
+
             }
 
             @Override
